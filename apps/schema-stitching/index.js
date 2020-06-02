d1fc8988863e3a821d5c670e82a5285bda35d41b
Siddharth Venkatesh <32375112+sidv93@users.noreply.github.com>
2020-06-03 01:11:06 +0530

    schema sticthing final

--- apps/schema-stitching/index.js
+++ apps/schema-stitching/index.js
@@ -4,41 +4,86 @@ import { stitchSchemas } from '@graphql-tools/stitch';
 import { fetch } from 'cross-fetch';
 import { print } from 'graphql';
 import { introspectSchema } from '@graphql-tools/wrap';
+import { delegateToSchema } from '@graphql-tools/delegate';
+import { wrapSchema } from '@graphql-tools/wrap';
+
+const schemaUrls = [
+    {
+        name: 'flight',
+        url: 'http://localhost:3000/graphql'
+    },
+    {
+        name: 'user',
+        url: 'http://localhost:2000/graphql'
+    }
+];
 
 const run = async () => {
-    const executor1 = async ({ document, variables }) => {
-        const query = print(document);
-        const fetchResult = await fetch('http://localhost:3000/graphql', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ query, variables })
-        });
-        return fetchResult.json();
-    };
+    const makeExecutor = async (url) => {
+        const executor = async ({ document, variables }) => {
+            const query = print(document);
+            const fetchResult = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ query, variables })
+            });
+            return fetchResult.json();
+        };
+        return executor;
+    }
 
-    const remoteSchema1 = await introspectSchema(executor1);
+    const makeSchema = async (executor) => {
+        const schema  = wrapSchema({
+            schema: await introspectSchema(executor),
+            executor
+        })
+        return schema;
+    }
 
-    const executor2 = async ({ document, variables }) => {
-        const query = print(document);
-        const fetchResult = await fetch('http://localhost:2000/graphql', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ query, variables })
-        });
-        return fetchResult.json();
-    };
+    const flightSchemaExecutor = await makeExecutor(schemaUrls[1].url);
+    const flightSchema = await makeSchema(flightSchemaExecutor);
 
-    const remoteSchema2 = await introspectSchema(executor2);
+    const userSchemaExecutor = await makeExecutor(schemaUrls[0].url);
+    const userSchema = await makeSchema(userSchemaExecutor);
 
+    const linkTypeDefs = `
+        extend type Flight {
+            passengers: [User]
+        }
+    `;
     const schema = stitchSchemas({
         subschemas: [
-            { schema: remoteSchema1, executor: executor1 },
-            { schema: remoteSchema2, executor: executor2 }
+            {
+                schema: flightSchema,
+                executor: flightSchemaExecutor
+            },
+            {
+                schema: userSchema,
+                executor: userSchemaExecutor
+            }
         ],
+        typeDefs: linkTypeDefs,
+        resolvers: {
+            Flight: {
+                passengers: {
+                    fragment: `... on Flight { id }`,
+                    resolve(flight, args, context, info) {
+                        return delegateToSchema({
+                            schema: userSchema,
+                            operation: 'query',
+                            fieldName: 'usersByFlight',
+                            args: {
+                                flightId: flight.id
+                            },
+                            context,
+                            info,
+                        });
+                    },
+                },
+            },
+        }
     });
     const server = new ApolloServer({
         schema
